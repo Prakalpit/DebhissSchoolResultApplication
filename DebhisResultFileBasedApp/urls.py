829605488dcdd830973b82a314bbb0279bf86b1c
prakalpit <prakalpit.sangram@gmail.com>
2022-11-19 12:10:37 +0545

    updated for hosting purposes

--- DebhisResultFileBasedApp/urls.py
+++ DebhisResultFileBasedApp/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('result.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
